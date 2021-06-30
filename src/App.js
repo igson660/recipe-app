95f81823c99f09406c89c373c63a9ced931947d8
Orlando Flores <orlando_flores@id.uff.br>
2021-06-30 12:19:15 -0300

    feat: prove o contexto de busca para a aplicação

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import { SearchBarContextProvider } from './contexts/searchBarContext';
 
 function App() {
   return (
     <Switch>
-      <Route exact path="/" component={ Login } />
+      <SearchBarContextProvider>
+        <Route exact path="/" component={ Login } />
+      </SearchBarContextProvider>
     </Switch>
   );
 }
