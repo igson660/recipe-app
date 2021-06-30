33c1234400442b8b7d7ff53156bc2ab5ed7f4b24
jlfagundes <jlfagundes@gmail.com>
2021-06-29 21:25:27 -0300

    feat: (req20) fixed footer on final page

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,23 +1,22 @@
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Footer.css';
 import iconDrinks from '../images/drinkIcon.svg';
 import iconMeals from '../images/mealIcon.svg';
 import iconExplorer from '../images/exploreIcon.svg';
 
 function Footer() {
   return (
-    <footer>
-      <Card.Footer data-testid="footer">
-        <img data-testid="drinks-bottom-btn" src={ iconDrinks } alt="Menu de Drinks" />
-        <img
-          data-testid="explore-bottom-btn"
-          src={ iconExplorer }
-          alt="Menu de Usuario"
-        />
-        <img data-testid="food-bottom-btn" src={ iconMeals } alt="Menu de Comidas" />
-      </Card.Footer>
-    </footer>
+    <Card.Footer data-testid="footer" className="footerClass">
+      <img data-testid="drinks-bottom-btn" src={ iconDrinks } alt="Menu de Drinks" />
+      <img
+        data-testid="explore-bottom-btn"
+        src={ iconExplorer }
+        alt="Menu de Usuario"
+      />
+      <img data-testid="food-bottom-btn" src={ iconMeals } alt="Menu de Comidas" />
+    </Card.Footer>
   );
 }
 
