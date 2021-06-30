33494178b30586fd8b630d87fbeebf00ed6e9429
igson <igson660@gmail.com>
2021-06-30 13:31:06 -0500

    feat(login screen): Muda o componeteLogin para Class e finaliza os quesitos dessa tela

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Recipes from './pages/Recipes';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
+      <Route path="/comidas" component={ Recipes } />
     </Switch>
   );
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,36 +1,70 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import { Redirect } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function Login() {
-  return (
-    <Form>
-      <Form.Group controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          data-testid="email-input"
-          type="email"
-          placeholder="Enter email"
-        />
-      </Form.Group>
-      <Form.Group controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          data-testid="password-input"
-          type="password"
-          placeholder="Password"
-        />
-      </Form.Group>
-      <Button
-        data-testid="login-submit-btn"
-        variant="primary"
-        type="submit"
-      >
-        Entrar
-      </Button>
-    </Form>
-  );
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      password: '',
+      isValidLogin: false,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    const { email } = this.state;
+    const pessoa = { email };
+    localStorage.setItem('mealsToken', 1);
+    localStorage.setItem('cocktailsToken', 1);
+    localStorage.setItem('user', JSON.stringify(pessoa));
+    this.setState({ isValidLogin: true });
+  }
+
+  render() {
+    const { email, password, isValidLogin } = this.state;
+    const quanty = 6;
+    return (
+      <>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control
+              data-testid="email-input"
+              type="email"
+              placeholder="Enter email"
+              value={ email }
+              onChange={ (e) => this.setState({ email: e.target.value }) }
+            />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              data-testid="password-input"
+              type="password"
+              placeholder="Password"
+              value={ password }
+              onChange={ (e) => this.setState({ password: e.target.value }) }
+            />
+          </Form.Group>
+          <Button
+            data-testid="login-submit-btn"
+            variant="primary"
+            type="submit"
+            onClick={ this.handleClick }
+            disabled={ !(password.length > quanty
+              && email.includes('.com') && email.includes('@')) }
+          >
+            Entrar
+          </Button>
+        </Form>
+        { isValidLogin ? <Redirect to="/comidas" /> : null }
+      </>
+    );
+  }
 }
 
 export default Login;
