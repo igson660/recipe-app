bf1ea5b92714aaa7bebea04b46706409f0105e48
Orlando Flores <orlando_flores@id.uff.br>
2021-07-01 19:00:16 -0300

    feat: cria contexto do header e o hook customizado

---
+++ src/contexts/headerContext.js
@@ -0,0 +1,20 @@
+import React, { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
+export const HeaderContext = createContext();
+
+export function HeaderContextProvider({ children }) {
+  const [toggleSearchBar, setToggleSearchBar] = useState(false);
+
+  return (
+    <HeaderContext.Provider
+      value={ { toggleSearchBar, setToggleSearchBar } }
+    >
+      {children}
+    </HeaderContext.Provider>
+  );
+}
+
+HeaderContextProvider.propTypes = {
+  children: PropTypes.node,
+}.isRequired;
