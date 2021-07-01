1b62664665cca76b380073822b640ab036092e37
Orlando Flores <orlando_flores@id.uff.br>
2021-07-01 19:01:02 -0300

    feat: adiciona logica para renderiza o serchBar quando for clicado o botao de search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import Alert from 'react-bootstrap/Alert';
 import useSearchBar from '../hooks/searchBar';
 import {
   searchRecipesByFirstLetter,
   searchRecipesByIngredients,
   searchRecipesByName,
 } from '../services/api';
+import useHeader from '../hooks/header';
 
 function SearchBar() {
   const [radioValue, setRadioValue] = useState('');
@@ -17,6 +17,7 @@ function SearchBar() {
     setRecipes,
     setLoading,
   } = useSearchBar();
+  const { toggleSearchBar } = useHeader();
 
   async function handleSearchButton() {
     setLoading(true);
@@ -46,71 +47,51 @@ function SearchBar() {
     }
     setLoading(false);
   }
-
-  // if (recipes === null) {
-  //   return (
-  //     <Alert
-  //       variant="warning"
-  //     >
-  //       Sinto muito, não encontramos nenhuma receita para esses filtros.
-  //     </Alert>);
-  // }
-
-  // if (recipes.length === 1) {
-  //   let id = 'idDrink';
-  //   if (pathname === '/comidas') {
-  //     id = 'idMeal';
-  //   }
-  //   return (
-  //     <Redirect to={ `${pathname}/${recipes[0][id]}` } />
-  //   );
-  // }
   return (
     <div>
-      <input
-        onChange={ ({ target }) => setSearchValue(target.value) }
-        type="text"
-        data-testid="search-input"
-      />
-      <input
-        type="radio"
-        value="ingredients"
-        data-testid="ingredient-search-radio"
-        name="search-bar-inputs"
-        onChange={ ({ target }) => setRadioValue(target.value) }
-      />
-      Ingrediente
-      <input
-        type="radio"
-        value="name"
-        data-testid="name-search-radio"
-        name="search-bar-inputs"
-        onChange={ ({ target }) => setRadioValue(target.value) }
-      />
-      Nome
-      <input
-        type="radio"
-        value="first-letter"
-        data-testid="first-letter-search-radio"
-        name="search-bar-inputs"
-        onChange={ ({ target }) => setRadioValue(target.value) }
-      />
-      Primeira letra
-      <button
-        type="button"
-        data-testid="exec-search-btn"
-        onClick={ handleSearchButton }
-        disabled={ !(radioValue && searchValue) }
-      >
-        Buscar
-      </button>
-      {alert
+      { toggleSearchBar
       && (
-        <Alert
-          variant="warning"
-        >
-          Sua busca deve conter somente 1 (um) caracter
-        </Alert>)}
+        <div>
+          <input
+            onChange={ ({ target }) => setSearchValue(target.value) }
+            type="text"
+            data-testid="search-input"
+          />
+          <input
+            type="radio"
+            value="ingredients"
+            data-testid="ingredient-search-radio"
+            name="search-bar-inputs"
+            onChange={ ({ target }) => setRadioValue(target.value) }
+          />
+          Ingrediente
+          <input
+            type="radio"
+            value="name"
+            data-testid="name-search-radio"
+            name="search-bar-inputs"
+            onChange={ ({ target }) => setRadioValue(target.value) }
+          />
+          Nome
+          <input
+            type="radio"
+            value="first-letter"
+            data-testid="first-letter-search-radio"
+            name="search-bar-inputs"
+            onChange={ ({ target }) => setRadioValue(target.value) }
+          />
+          Primeira letra
+          <button
+            type="button"
+            data-testid="exec-search-btn"
+            onClick={ handleSearchButton }
+            disabled={ !(radioValue && searchValue) }
+          >
+            Buscar
+          </button>
+          {alert && window.alert('Sua busca deve conter somente 1 (um) caracter') }
+        </div>
+      ) }
     </div>
   );
 }
